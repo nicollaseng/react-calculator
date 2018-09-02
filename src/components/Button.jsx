3cbad7e48f15424dc676714c5bffb1307510d5c7
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-09-02 16:55:09 -0300

    added operations to buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
-import './Button.css'
+import React from "react";
+import "./Button.css";
 
-export default props => <button className="button">{props.label}</button>
+export default props => {
+  return (
+    <button
+    onClick={event => props.click && props.click(props.label)}
+      className={`button
+    ${props.operation ? "operation" : ""}
+    ${props.double ? "double" : ""}
+    ${props.triple ? "triple" : ""}`}
+    >
+      {props.label}
+    </button>
+  );
+};
